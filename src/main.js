647853da6e456079ba2ba39f8c2c44b78647266d
jgrossi01 <juliangrossi@hotmail.com>
2022-07-21 16:58:20 -0300

    #

--- src/main.js
+++ src/main.js
@@ -4,16 +4,16 @@ import { createLi, loadCards } from "./content.js";
 window.addEventListener("DOMContentLoaded", (event) => {
   createLi();
   loadCards();
+
+    const bookingForm = document.getElementById("bookingForm");
+    const clearFormBtn = document.getElementById("clearFormBtn");
+
+    clearFormBtn.addEventListener("click", () => {
+    bookingForm.reset();
+    });
+
+    bookingForm.addEventListener("submit", formValidate);
 });
-
-const bookingForm = document.getElementById("bookingForm");
-const clearFormBtn = document.getElementById("clearFormBtn");
-
-clearFormBtn.addEventListener("click", () => {
-  bookingForm.reset();
-});
-
-bookingForm.addEventListener("submit", formValidate);
 
 function nextIndexOf(array) {
   return array.length + 1;
